d30f3f548222a853cd4dd8c95cd4f50267927572
cx559824 <cb_r12@yahoo.com>
2025-08-29 13:56:40 +0800

    end of timestamp

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,6 +1,22 @@
-const GsapFromTo = () => {
-  // TODO: Implement the gsap.fromTo() method
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
+const GsapFromTo = () => {
+  useGSAP(() => {
+    gsap.fromTo("#red-box", {
+      x: 0,
+      rotation: 0,
+      borderRadius: "0%"
+    }, {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      borderRadius: "100%",
+      rotation: 360,
+      duration: 2,
+      ease: 'bounce.out'
+    })
+  }, []);
   return (
     <main>
       <h1>GsapFromTo</h1>
